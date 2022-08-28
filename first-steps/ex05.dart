037dfff58e856b69796b73e8d0073bbd2dd3d56e
Konitsu <89197623+Konitsu@users.noreply.github.com>
2022-08-28 20:31:25 +0000

    Commit ex05

--- first-steps/ex05.dart
+++ first-steps/ex05.dart
@@ -5,3 +5,20 @@ correspondente em graus Celsius (baseado na fórmula abaixo):
   C / 5 = (F - 32) / 9 
   Observação: Para testar se a sua resposta está correta saiba que 100ºC = 212F
 */
+
+import 'dart:io';
+
+void main() {
+  print("Insira a temperatura em Fahrenheit: ");
+  double Fahrenheit = double.parse(stdin.readLineSync()!);
+
+  double FahrenheitToCelsius = ConvertFahrenheitToCelsius(Fahrenheit);
+  print("$Fahrenheit\F = $FahrenheitToCelsius\C");
+}
+
+double ConvertFahrenheitToCelsius(Fahrenheit) {
+  double Celsius;
+  Celsius = ((Fahrenheit - 32) * 5) / 9;
+
+  return Celsius;
+}
